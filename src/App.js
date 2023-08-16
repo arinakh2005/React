d574eee33091cca8f3173517a0f79f22b728799c
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-16 15:29:01 +0300

    feat: add auth state to Redux store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
+import Auth from './components/Auth';
+import Header from './components/Header';
 import Counter from './components/Counter';
 
-
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </>
   );
 }
 
