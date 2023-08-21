163a51e08fdd2a46735cc787567b0e7c6582e407
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-21 17:13:14 +0300

    feat[frontend]: display error messages (refers to form) from API

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,7 +1,8 @@
-import { Form, useNavigate, useNavigation } from 'react-router-dom';
+import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom';
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
@@ -12,6 +13,13 @@ function EventForm({ method, event }) {
 
   return (
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((error) =>
+            <li key={error}>{error}</li>
+          )}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
