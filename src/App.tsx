93004b38687ed4de6a3195db04b5f97d6db126e9
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-24 11:39:13 +0300

    feat: add props with type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Todos items={['Learn React', 'Learn TypeScript']} />
+    </div>
   );
 }
 
