174112bc6c731f745e20360165ab2d3db3721666
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-24 21:53:35 +0300

    feat: ability to remove Ingredient

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,11 +6,17 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
       ...prevIngredients,
       { id: Math.random().toString(), ...ingredient },
     ]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId),
+    );
   };
 
   return (
@@ -19,7 +25,10 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
