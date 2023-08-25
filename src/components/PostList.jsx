23e2e0908f0e888c92cfa7944bb0e402d1592941
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-25 15:12:15 +0300

    improvement: add routing loader

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Post from './Post.jsx';
 import classes from './PostsList.module.css';
 
 function PostList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts () {
-      setIsFetching(true);
-
-      const response = await fetch('http://localhost:8080/posts');
-      const resData = await response.json();
-
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
@@ -34,22 +19,17 @@ function PostList() {
 
   return (
     <>
-      {!isFetching && posts.length &&
+      {posts.length &&
         <ul className={classes.posts}>
           {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
         </ul>
       }
-      {!isFetching && !posts.length &&
+      {!posts.length &&
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       }
-      {isFetching && (
-        <div style={{ textAlign: 'center', color: 'white' }}>
-          <p>Loading posts...</p>
-        </div>
-      )}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Posts from './routes/Posts.jsx'
+import Posts, { loader as postLoader } from './routes/Posts.jsx';
 import './index.css'
 import NewPost from './routes/NewPost.jsx';
 import RootLayout from './routes/RootLayout.jsx';
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Posts />,
+        loader: postLoader,
         children: [
           { path: '/create-post', element: <NewPost /> },
         ],
